3f5fff07c82f3c3fc08948914a7edd42a1bdf548
Patrick McClellan <pmcclellan1289@gmail.com>
2019-02-18 15:05:07 -0600

    Chatbot get burn center commands

---
+++ Sprinkles.py
@@ -0,0 +1,22 @@
+import random
+import csv
+
+
+def get_burn_center():
+    with open('/home/patrick/Downloads/burn_centers.csv') as csv_file:
+        reader = csv.reader(csv_file)
+        rownum = random.randrange(0, 159, 1)
+        rows = list(reader)
+        print(str(rows[rownum]))
+
+
+def handle_command(command):
+    command = command.lower()
+
+    if command == "!getalistofburncenters":
+        print("Ouch. Here's a burn center for you to contact: \n")
+        get_burn_center()
+
+
+if __name__ == '__main__':
+    handle_command(input("Enter command: "))
